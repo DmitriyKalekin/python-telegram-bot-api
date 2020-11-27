cb3f2fea1f8920e2ac67508c9c2b8604347cb0b4
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-27 15:00:36 +0600

    sh file changed

--- build-package.sh
+++ build-package.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
-# rm -rf ./build
-# rm -rf ./dist
-# rm -rf ./py3tgbot.egg-info
+
+echo  "run script"
+rm -rf ./build
+rm -rf ./dist
+rm -rf ./py3tgbot.egg-info
 python3 setup.py sdist bdist_wheel
 twine upload --repository testpypi dist/*
 twine upload --repository pypi dist/*
