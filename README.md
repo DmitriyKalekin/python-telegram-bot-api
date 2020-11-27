6274fbbf3097cec0d1495b6eb542602dbd9a682c
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-27 15:40:51 +0600

    typing in methods, readme fix

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 <img src="https://img.shields.io/badge/tests-pytest-orange?style=for-the-badge" alt="pytest"/>
 <img src="https://img.shields.io/badge/async-asyncio, aiohttp-green?style=for-the-badge" alt="asyncio, aiohttp"/>
 <a href="https://t.me/herr_horror"><img src="https://img.shields.io/badge/Telegram Chat-@herr_horror-2CA5E0.svg?logo=telegram&style=for-the-badge" alt="Chat on Telegram"/></a>
-<img src="https://img.shields.io/badge/version-v.0.0.2-green?style=for-the-badge" alt="Last Release"/>
+<img src="https://img.shields.io/badge/version-v.0.0.3-green?style=for-the-badge" alt="Last Release"/>
 </p>
 
 
@@ -34,11 +34,11 @@ client_tgbot = TgBotJson(token=TGBOT_TOKEN)
 
 
 async def main_async():
-    response = await app.client_tgbot.setWebhook("{hostname}/tgbot/wh".format(hostname=APP_HOSTNAME))
+    response = await client_tgbot.setWebhook("{hostname}/tgbot/wh".format(hostname=APP_HOSTNAME))
     r = await response.json()
     print(r)
 
-    response = await app.client_tgbot.sendMessage(CHAT_ID, "Hello from Telegram Bot!")
+    response = await client_tgbot.sendMessage(CHAT_ID, "Hello from Telegram Bot!")
     r = await response.json()
     print(r)
 
--- api_tgbot/tgbot_json.py
+++ api_tgbot/tgbot_json.py
@@ -9,18 +9,19 @@ class TgBotJson:
     def __repr__(self):
         return self.url   # pragma: no cover
 
-    async def getUpdates(self, json: dict = None) -> ClientResponse:
+    async def getUpdates(self, limit: int = 100, **kwargs) -> ClientResponse:
         url = self.url + "getUpdates"
-        async with ClientSession() as c:
-            return await c.get(url)
+        params = {'limit': limit, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.get(url, json=params)
 
-    async def sendMessage(self, chat_id, text, **kwargs) -> ClientResponse:
+    async def sendMessage(self, chat_id: int, text: str, **kwargs) -> ClientResponse:
         assert text and text != ""
         if "parse_mode" not in kwargs and "@" not in text:
             kwargs["parse_mode"] = "markdown"
         url = self.url + 'sendMessage'
         params = {'chat_id': chat_id, 'text': text, **kwargs}
-        async with ClientSession() as c:
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
             response = await c.post(url, json=params)
             r = await response.json()
             if response.status == 400 and "can't parse entities" in r.get("description", ""):
@@ -29,66 +30,7 @@ class TgBotJson:
                 return response2
             return response
 
-    # async def sendPhoto(self, chat_id, **kwargs) -> ClientResponse:
-    #     """
-    #     @example photo="http://www.aisystems.ru/temp/hor/img/logo.png",
-    #     caption="<b>Возвращение квантового кота</b>", parse_mode="html"
-    #     """
-    #     url = self.url + 'sendPhoto'
-    #     params = {'chat_id': chat_id, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
-    #
-    # async def sendMediaGroup(self, chat_id, media: list, **kwargs) -> ClientResponse:
-    #     """
-    #     @example photo="http://www.aisystems.ru/temp/hor/img/logo.png",
-    #     caption="<b>Возвращение квантового кота</b>", parse_mode="html"
-    #     """
-    #     url = self.url + 'sendMediaGroup'
-    #     params = {'chat_id': chat_id, "media": media, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
-    #
-    # async def sendAnimation(self, chat_id, **kwargs) -> ClientResponse:
-    #     url = self.url + 'sendAnimation'
-    #     params = {'chat_id': chat_id, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
-    #
-    # async def sendVideo(self, chat_id, **kwargs) -> ClientResponse:
-    #     url = self.url + 'sendVideo'
-    #     params = {'chat_id': chat_id, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
-
-    async def setWebhook(self, wh_url) -> ClientResponse:
-        url = self.url + "setWebhook?url=" + wh_url
-        async with ClientSession() as c:
-            return await c.get(url)
-
-    async def getWebhookInfo(self) -> ClientResponse:
-        url = self.url + "getWebhookInfo"
-        async with ClientSession() as c:
-            return await c.get(url)
-
-    async def deleteWebhook(self) -> ClientResponse:
-        url = self.url + "deleteWebhook"
-        async with ClientSession() as c:
-            return await c.get(url)
-
-    # async def getChatAdministrators(self, chat_id, **kwargs) -> ClientResponse:
-    #     url = self.url + 'getChatAdministrators'
-    #     params = {'chat_id': chat_id, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
-
-    async def deleteMessage(self, chat_id, message_id) -> ClientResponse:
-        url = self.url + 'deleteMessage'
-        params = {'chat_id': chat_id, 'message_id': message_id}
-        async with ClientSession() as c:
-            return await c.post(url, json=params)
-
-    async def editMessageText(self, chat_id, message_id, text, **kwargs) -> ClientResponse:
+    async def editMessageText(self, chat_id: int, message_id: int, text: str, **kwargs) -> ClientResponse:
         if "parse_mode" not in kwargs and "@" not in text:
             kwargs["parse_mode"] = "markdown"
         url = self.url + 'editMessageText'
@@ -102,15 +44,62 @@ class TgBotJson:
                 return response2
             return response
 
+    async def deleteMessage(self, chat_id: int, message_id: int) -> ClientResponse:
+        url = self.url + 'deleteMessage'
+        params = {'chat_id': chat_id, 'message_id': message_id}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
 
-    # async def editMessageMedia(self, chat_id, message_id, media, **kwargs) -> ClientResponse:
-    #     url = self.url + 'editMessageMedia'
-    #     params = {'chat_id': chat_id, 'message_id': message_id, 'media': media, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
+    async def sendPhoto(self, chat_id: int, caption: str = "", **kwargs) -> ClientResponse:
+        if "parse_mode" not in kwargs:
+            kwargs["parse_mode"] = "html"
+        url = self.url + 'sendPhoto'
+        params = {'chat_id': chat_id, 'caption': caption, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
 
-    # async def paramsCallbackQuery(self, callback_query_id, text, **kwargs) -> ClientResponse:
-    #     url = self.url + 'paramsCallbackQuery'
-    #     params = {'callback_query_id': callback_query_id, 'text': text, **kwargs}
-    #     async with ClientSession() as c:
-    #         return await c.post(url, json=params)
+    async def sendMediaGroup(self, chat_id: int, media: list, **kwargs) -> ClientResponse:
+        url = self.url + 'sendMediaGroup'
+        params = {'chat_id': chat_id, "media": media, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
+
+    async def sendAnimation(self, chat_id: int, **kwargs) -> ClientResponse:
+        url = self.url + 'sendAnimation'
+        params = {'chat_id': chat_id, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
+
+    async def sendVideo(self, chat_id: int, **kwargs) -> ClientResponse:
+        url = self.url + 'sendVideo'
+        params = {'chat_id': chat_id, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
+
+    async def setWebhook(self, wh_url: str) -> ClientResponse:
+        url = self.url + "setWebhook?url=" + wh_url
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.get(url)
+
+    async def getWebhookInfo(self) -> ClientResponse:
+        url = self.url + "getWebhookInfo"
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.get(url)
+
+    async def deleteWebhook(self) -> ClientResponse:
+        url = self.url + "deleteWebhook"
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.get(url)
+
+    async def getChatAdministrators(self, chat_id: int, **kwargs) -> ClientResponse:
+        url = self.url + 'getChatAdministrators'
+        params = {'chat_id': chat_id, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
+
+    async def editMessageMedia(self, chat_id: int, message_id: int, media: dict, **kwargs) -> ClientResponse:
+        url = self.url + 'editMessageMedia'
+        params = {'chat_id': chat_id, 'message_id': message_id, 'media': media, **kwargs}
+        async with ClientSession(headers={"Accept": "application/json"}) as c:
+            return await c.post(url, json=params)
+
