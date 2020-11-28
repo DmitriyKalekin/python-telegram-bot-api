a353c2b83f22f240f571ccb6b8e1bdbb25f482c2
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-28 14:07:26 +0600

    setup: requirements from file + pygramtic package

--- build-package.sh
+++ build-package.sh
@@ -5,5 +5,5 @@ rm -rf ./build
 rm -rf ./dist
 rm -rf ./py3tgbot.egg-info
 python3 setup.py sdist bdist_wheel
-twine upload --repository testpypi dist/*
-twine upload --repository pypi dist/*
+echo "DmitriyKalekin\nNeverstop37!" | twine upload --repository testpypi dist/*
+echo "DmitriyKalekin\nNeverstop37!" |twine upload --repository pypi dist/*
--- setup.py
+++ setup.py
@@ -1,28 +1,42 @@
-"""Setup script for telegram-bot-api"""
-import setuptools
+"""Setup script for python-telegram-bot-api"""
+from os import path
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-	long_description = fh.read()
+here = path.abspath(path.dirname(__file__))
 
-requirements = ["aiohttp>=3.6.2"]
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-setuptools.setup(
-	name="python-telegram-bot-api",
-	version="0.0.3",
-	author="Dmitriy Kalekin",
-	author_email="herrhorror@gmail.com",
-	description="Simple and fast client to call rest-api endpoints `api.telegram.org` using `aiohttp` package.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/DmitriyKalekin/python-telegram-bot-api",
-	packages=setuptools.find_packages(),
-	classifiers=[
-		"Programming Language :: Python :: 3.8",
-		"License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-		"Operating System :: OS Independent",
-	],
-	install_requires=[
-		"aiohttp"
-	],
-	python_requires='>=3.7',
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+with open(path.join(here, 'VERSION')) as f:
+    version = f.read().strip()
+
+requirements = ["aiohttp>=3.6.2", "pygramtic>=0.2.0"]
+
+setup(
+    name="python-telegram-bot-api",
+    version=version,
+    author="Dmitriy Kalekin",
+    author_email="herrhorror@gmail.com",
+    description="Simple and fast client to call rest-api endpoints `api.telegram.org` using `aiohttp` package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DmitriyKalekin/python-telegram-bot-api",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    keywords='pydantic telegram api bot',
+    classifiers=[
+        # "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required_packages,
+    python_requires='>=3.7, <4',
+    project_urls={
+        "Source": "https://github.com/DmitriyKalekin/python-telegram-bot-api",
+    },
 )
