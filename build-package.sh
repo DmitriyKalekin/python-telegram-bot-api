7b0f5c11fa21b24cdc9d5fbf200a500f70c4fe2a
Dmitriy Kalekin <herrhorror@gmail.com>
2020-11-28 14:15:02 +0600

    Delete build-package.sh

--- build-package.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-echo  "run script"
-rm -rf ./build
-rm -rf ./dist
-rm -rf ./py3tgbot.egg-info
-python3 setup.py sdist bdist_wheel
-echo "DmitriyKalekin\nNeverstop37!" | twine upload --repository testpypi dist/*
-echo "DmitriyKalekin\nNeverstop37!" |twine upload --repository pypi dist/*
